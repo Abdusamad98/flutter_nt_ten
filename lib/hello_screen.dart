4cc082ab92cc5bfefc59991200cfa468cb93a56d
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2024-02-13 17:05:11 +0500

    Some changes

--- lib/hello_screen.dart
+++ lib/hello_screen.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_nt_ten/utils/colors/app_colors.dart';
+import 'package:http/http.dart' as http;
 
-
-class HelloScreen extends StatelessWidget {
+class HelloScreen extends StatefulWidget {
   const HelloScreen({super.key});
+
+  @override
+  State<HelloScreen> createState() => _HelloScreenState();
+}
+
+class _HelloScreenState extends State<HelloScreen> {
+  Future<http.Response> fetchAlbum() {
+    return http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/'));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.white,
-      appBar: AppBar(title: Text("Deafult"),),
-      body: Center(child: const Text("HEllO"),),
+      appBar: AppBar(
+        title: Text("Deafult"),
+      ),
+      body: Center(
+          child: TextButton(
+        onPressed: () {
+          fetchAlbum();
+        },
+        child: Text("Call API"),
+      )),
     );
   }
 }
