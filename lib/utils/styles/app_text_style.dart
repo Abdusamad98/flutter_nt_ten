3c497aafd6408a2238bbd08b9da1c1a54f4b9d1b
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2024-02-13 16:00:23 +0500

    Some changes

--- lib/utils/styles/app_text_style.dart
+++ lib/utils/styles/app_text_style.dart
@@ -1,66 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_nt_ten/utils/colors/app_colors.dart';
+import 'package:flutter_nt_ten/utils/size/size_utils.dart';
 
 class AppTextStyle {
   //Inter
-  static const TextStyle interThin = TextStyle(
+  static  TextStyle interThin = TextStyle(
     fontFamily: "Inter",
     fontWeight: FontWeight.w200,
-    fontSize: 14,
+    fontSize: 14.w,
     color: AppColors.c_2A3256,
   );
-  static const TextStyle interLight = TextStyle(
+  static  TextStyle interLight = TextStyle(
     fontFamily: "Inter",
     fontWeight: FontWeight.w300,
-    fontSize: 14,
+    fontSize: 14.w,
     color: AppColors.c_2A3256,
   );
-  static const TextStyle interRegular = TextStyle(
+  static  TextStyle interRegular = TextStyle(
     fontFamily: "Inter",
     fontWeight: FontWeight.w400,
-    fontSize: 16,
+    fontSize: 16.w,
     color: AppColors.c_2A3256,
   );
-  static const TextStyle interMedium = TextStyle(
+  static  TextStyle interMedium = TextStyle(
     fontFamily: "Inter",
     fontWeight: FontWeight.w500,
-    fontSize: 16,
+    fontSize: 16.w,
     color: AppColors.c_2A3256,
   );
 
-  static const TextStyle interSemiBold = TextStyle(
+  static  TextStyle interSemiBold = TextStyle(
     fontFamily: "Inter",
     fontWeight: FontWeight.w600,
-    fontSize: 16,
+    fontSize: 16.w,
     color: AppColors.c_2A3256,
   );
 
-  static const TextStyle interBold = TextStyle(
+  static  TextStyle interBold = TextStyle(
     fontFamily: "Inter",
     fontWeight: FontWeight.w700,
-    fontSize: 16,
+    fontSize: 16.w,
     color: AppColors.c_2A3256,
   );
 
-  static const TextStyle interBlack = TextStyle(
+  static  TextStyle interBlack = TextStyle(
     fontFamily: "Inter",
     fontWeight: FontWeight.w800,
-    fontSize: 16,
-    color: AppColors.c_2A3256,
-  );
-
-  //Bodoni
-  static const TextStyle bodoniBold = TextStyle(
-    fontFamily: "Bodoni",
-    fontWeight: FontWeight.w700,
-    fontSize: 14,
-    color: AppColors.c_2A3256,
-  );
-
-  static const TextStyle bodoniRegular = TextStyle(
-    fontFamily: "Bodoni",
-    fontWeight: FontWeight.w400,
-    fontSize: 16,
+    fontSize: 16.w,
     color: AppColors.c_2A3256,
   );
 }
