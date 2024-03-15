bc18a1a0fc3f381a3ee85efdd2800bcf7f0f4f5e
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2024-03-15 17:59:54 +0500

    Some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_nt_ten/data/local/storage_repository.dart';
+import 'package:flutter_nt_ten/data/repositories/book_repo.dart';
 import 'package:flutter_nt_ten/screens/library/library_screen.dart';
 import 'package:flutter_nt_ten/utils/theme/app_theme.dart';
+import 'package:flutter_nt_ten/view_models/book_view_model.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await StorageRepository.init();
   runApp(MultiProvider(
-    providers: [],
+    providers: [
+      ChangeNotifierProvider(
+          create: (_) => BookViewModel(productRepo: BookRepo()))
+    ],
     child: MyApp(),
   ));
 }
--- lib/screens/library/library_screen.dart
+++ lib/screens/library/library_screen.dart
@@ -1,10 +1,26 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+
+String imageUrl =
+    "https://purepng.com/public/uploads/large/purepng.com-booksbookillustratedwrittenprintedliteratureclipart-1421526451707uyace.png";
 
 class LibraryScreen extends StatelessWidget {
   const LibraryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: CachedNetworkImage(
+          imageUrl: imageUrl,
+          placeholder: (context, url) =>
+              const Center(child: CircularProgressIndicator()),
+          errorWidget: (context, url, error) => const Icon(Icons.error),
+          width: 300,
+          height: 300,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
   }
 }
