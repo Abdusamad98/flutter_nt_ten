baf4fcfb3cfc5ccffac86eb729a955a398be801d
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2024-04-08 20:39:57 +0500

    Some changes

--- lib/utils/constants/app_constants.dart
+++ lib/utils/constants/app_constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
   static String baseUrl = "https://crudapi.co.uk";
-  static String token = "aw4WWbedmDumEnhRAu7h6oiHoo8C-JIcCdyN9yTToeXm1uqDvA";
+  static String token = "";
 
 }
